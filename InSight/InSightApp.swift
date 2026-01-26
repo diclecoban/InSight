8acc256caa5dc802dd1c460a952a9ee9d57975c0
diclecoban <d.coban2022@gtu.edu.tr>
2026-01-26 04:50:23 +0300

    Welcome page has started

---
+++ InSight/InSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InSightApp.swift
+//  InSight
+//
+//  Created by Dicle Sara Çoban on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct InSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InSight/SignIn.swift
@@ -0,0 +1,18 @@
+//
+//  SignIn.swift
+//  InSight
+//
+//  Created by Dicle Sara Çoban on 26.01.2026.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignIn()
+}
